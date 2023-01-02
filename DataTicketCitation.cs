1c41912c2d396fd2ab5d6e13bb9df7fbebbb55eb
Jarrett <jarrettboice@gmail.com>
2023-01-01 16:59:41 -0800

    remove api

--- DataTicketCitation.cs
+++ DataTicketCitation.cs
@@ -37,17 +37,17 @@ namespace SBPD_DT_Sentinel
             using (WebClient wc = new WebClient())
             {
                 string bingLocation = location + ", Seal Beach CA 90740";
-                var json = wc.DownloadString("http://dev.virtualearth.net/REST/V1/Routes/Walking?wp.0=" + bingLocation + "&wp.1=" + "215 Main St, Seal Beach CA 90740" + "&key=KqIPiiPICvJvqRalm1Y9~yDGiUeoW_JI27wt0hI3x6w~Aqe8NJcoZIoyYT_dVxh61HQLUN10dVMO-YQXEF6o_Nj21Vhqq0GGnmTZuBd6BatK");
+                var json = wc.DownloadString("http://dev.virtualearth.net/REST/V1/Routes/Walking?wp.0=" + bingLocation + "&wp.1=" + "215 Main St, Seal Beach CA 90740" + "&key=");
                 dynamic jsonObj = JsonConvert.DeserializeObject(json);
                 double travelDistance = jsonObj.resourceSets[0].resources[0].travelDistance;
                 geoZoneDistances.Add(geoZone.oldTown, travelDistance);
 
-                json = wc.DownloadString("http://dev.virtualearth.net/REST/V1/Routes/Walking?wp.0=" + bingLocation + "&wp.1=" + "13900 Seal Beach Blvd, Seal Beach CA 90740" + "&key=KqIPiiPICvJvqRalm1Y9~yDGiUeoW_JI27wt0hI3x6w~Aqe8NJcoZIoyYT_dVxh61HQLUN10dVMO-YQXEF6o_Nj21Vhqq0GGnmTZuBd6BatK");
+                json = wc.DownloadString("http://dev.virtualearth.net/REST/V1/Routes/Walking?wp.0=" + bingLocation + "&wp.1=" + "13900 Seal Beach Blvd, Seal Beach CA 90740" + "&key=");
                 jsonObj = JsonConvert.DeserializeObject(json);
                 travelDistance = jsonObj.resourceSets[0].resources[0].travelDistance;
                 geoZoneDistances.Add(geoZone.northEnd, travelDistance);
 
-                json = wc.DownloadString("http://dev.virtualearth.net/REST/V1/Routes/Walking?wp.0=" + bingLocation + "&wp.1=" + "1430 Catalina Ave, Seal Beach CA 90740" + "&key=KqIPiiPICvJvqRalm1Y9~yDGiUeoW_JI27wt0hI3x6w~Aqe8NJcoZIoyYT_dVxh61HQLUN10dVMO-YQXEF6o_Nj21Vhqq0GGnmTZuBd6BatK");
+                json = wc.DownloadString("http://dev.virtualearth.net/REST/V1/Routes/Walking?wp.0=" + bingLocation + "&wp.1=" + "1430 Catalina Ave, Seal Beach CA 90740" + "&key=");
                 jsonObj = JsonConvert.DeserializeObject(json);
                 travelDistance = jsonObj.resourceSets[0].resources[0].travelDistance;
                 geoZoneDistances.Add(geoZone.theHill, travelDistance);
